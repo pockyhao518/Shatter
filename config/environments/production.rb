cdda6c63417555a6d6c2e5602bdafdabea76c8e0
Hao <pockyhao518@gmail.com>
2021-01-21 14:31:01 -0500

    background

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -37,7 +37,8 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
+  config.serve_static_assets = true
+  config.assets.digest = true
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
